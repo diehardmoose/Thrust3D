291d284aa83acafb36d96905331011214fc1eca5
diehardmoose <37848492+diehardmoose@users.noreply.github.com>
2018-04-04 21:30:25 +0100

    MinimapV1
    Added minimap V1
    set y rotation restraint on ship

---
+++ Assets/Scripts/MiniMapCameraController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapCameraController : MonoBehaviour {
+    public GameObject ObjectToFollow;
+    public float offsetX;
+    public float offsetY;
+    public float offsetZ;
+
+    public Vector3 offset;
+    public Vector3 testpos;
+
+    // Use this for initialization
+    void Start () {
+        offset.x = offsetX;
+        offset.y = offsetY;
+        offset.z = offsetZ;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = ObjectToFollow.transform.position + offset;
+        
+        testpos = transform.position;
+
+
+    }
+}
